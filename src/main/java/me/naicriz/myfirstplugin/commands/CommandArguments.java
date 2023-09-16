178bfddfd1fdaf6152f97aac7aafcf50c60b8656
Ian S <isalazarjara@gmail.com>
2023-09-15 21:11:01 -0300

    Update CommandsArguments P2
    - Trying different things.
    - For loops, string builder & more.

--- src/main/java/me/naicriz/myfirstplugin/commands/CommandArguments.java
+++ src/main/java/me/naicriz/myfirstplugin/commands/CommandArguments.java
@@ -15,13 +15,33 @@ public class CommandArguments implements CommandExecutor {
         // No sé si es lo más eficiente, pero se verá más adelante.
         if (sender instanceof Player player) {
             switch ( args.length ) {
-                case 0 -> player.sendMessage("Sin argumentos proporcionados.\n" + ChatColor.AQUA + "/cmdargs <args>");
-                case 1 -> player.sendMessage("Argumento proporcionado:" + args.length + " " + args[0]);
-                default -> {
-                    player.sendMessage("Argumentos proporcionados: " + args.length);
+                case 0 -> player.sendMessage(ChatColor.BOLD + "Sin argumentos proporcionados.\n" + ChatColor.AQUA + ChatColor.BOLD + "/cmdargs <args>");
+                case 1 -> player.sendMessage(ChatColor.BOLD + "Argumento proporcionado: " + ChatColor.BOLD + ChatColor.GOLD + args.length + " " + args[0]);
+                case 2 -> {
+                    String word = args[1];
+                    player.sendMessage( ChatColor.GREEN + "Case 2 | " + ChatColor.WHITE + ChatColor.BOLD + "Argumentos proporcionados: " + ChatColor.BOLD + ChatColor.GOLD + args.length);
+                    player.sendMessage(ChatColor.BOLD + "Argumento: " + ChatColor.BOLD + ChatColor.GOLD + args.length + ChatColor.AQUA + ChatColor.BOLD + word);
+                }
+                case 3 -> {
+                    player.sendMessage(ChatColor.BOLD + "Argumentos proporcionados: "+ ChatColor.BOLD + ChatColor.GOLD  + args.length);
                     for (int i = 0; i < args.length; i++) {
-                        player.sendMessage(ChatColor.BOLD + "Argumento " + ChatColor.BOLD + ChatColor.GOLD + i + ChatColor.BOLD + ": " + ChatColor.AQUA + args[i]);
+                        player.sendMessage(ChatColor.BOLD + "Argumento: " + ChatColor.BOLD + ChatColor.GOLD + i + ChatColor.BOLD + ": " + ChatColor.AQUA + args[i]);
                     }
+                }
+                default -> {
+                    StringBuilder strbuilder = new StringBuilder();
+
+                    // for (int i = 0; i < args.length; i++) {
+                        // strbuilder.append(args[i]);
+                        // strbuilder.append(" ");
+                    // }
+                    for (String arg : args) {
+                        strbuilder.append(arg).append(" ");
+                    }
+                    // return the strbuilder as a string
+                    String str = strbuilder.toString().stripTrailing();
+                    player.sendMessage(ChatColor.BOLD + "Este es todo el string: " + ChatColor.BOLD + ChatColor.AQUA + str);
+
                 }
             }
         }
