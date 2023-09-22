5c91da780969c24304753d6546a1c6b97f54ecd8
Ian S <isalazarjara@gmail.com>
2023-09-22 18:04:16 -0300

    Refactor SetMessageCmd
    - Refactor of all the code, now its asynchronous and should be better in performance.
    - Also thelogic has been change a bit.
    - This is just used as example.

--- src/main/java/me/naicriz/myfirstplugin/commands/SetMessageCommand.java
+++ src/main/java/me/naicriz/myfirstplugin/commands/SetMessageCommand.java
@@ -3,12 +3,43 @@ package me.naicriz.myfirstplugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
 
 import static me.naicriz.myfirstplugin.MyFirstPlugin_MC.getPlugin;
 
 public class SetMessageCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Metodo v2 Async
+        // Este método NO debe ser usado en COMDANDOS de este tipo, solo en tareas pesadas u operaciones que puedan tardar.
+        //
+        if (args[0].equalsIgnoreCase("joinmsg")) {
+            if (args.length >= 2) {
+                // Combina los argumentos restantes en un mensaje
+                String finalMessageString = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+
+                // Programa una tarea asíncrona para guardar la configuración
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        getPlugin().getConfig().set("join-message-player", finalMessageString);
+                        getPlugin().saveConfig();
+                    }
+                }.runTaskAsynchronously(getPlugin());
+
+                sender.sendMessage("Mensaje de bienvenida configurado de forma asíncrona.");
+            } else {
+                sender.sendMessage("Uso incorrecto. Debes proporcionar un mensaje de bienvenida.");
+            }
+        }
+        return true;
+    }
+}
+
+// Metodo v1
+/*public class SetMessageCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -23,4 +54,4 @@ public class SetMessageCommand implements CommandExecutor {
         }
     return true;
     }
-}
+}*/
